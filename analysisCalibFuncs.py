bd5cb7b6db91704a488475317ca3f47bacf12a03
Annapaola <annapaola@pb-d-128-141-62-104.cern.ch>
2014-04-15 16:07:17 +0200

    improved VcThr-Vcal fit + added function to get key and dac for SCurve calib

--- analysisCalibFuncs.py
+++ analysisCalibFuncs.py
@@ -39,58 +39,59 @@ import ROOT
 from array import array
 
 
-def fitVcalVcThr(path,  savePlots):
 
+#dacdir      = os.environ['PIXELCONFIGURATIONBASE']+'/dac'
+confpath    = "Run_1000/configurations.txt"
+
+def fitVcalVcThr(path,  savePlots):
+    failingRocs = 0 
     if not os.path.isfile(path + 'mapRocVcalVcThr.txt'):
         ofile = open(path + 'mapRocVcalVcThr.txt', 'w')
         ofile.write('='*60)    
         ofile.write('\nROC name                              a      b     chi2/NDF \n')
         ofile.write('='*60)
-        ofile.write('\nVcal = a + b*VcThr \n')
+        ofile.write('\nVcThr = a + b*Vcal \n')
         ofile.write('='*60)
     else: ofile = open(path + 'mapRocVcalVcThr.txt', 'a')
  
     for roc in ROOT.gDirectory.GetListOfKeys(): # ROCs, e.g.:  BmI_SEC4_LYR1_LDR5F_MOD1_ROC0
         cName =  roc.GetName()
         h = roc.ReadObj().GetPrimitive(cName)
-        step = h.GetBinWidth(1)
+        step = h.GetYaxis().GetBinWidth(1)
         ### Fit range 
-        VcThrMin = 40
-        VcThrMax = 120    
+        VcalMin = 35
+        VcalMax = 110    
         # define the two arrays VcThrs and Vcals,
         # the former storing the values of VcThr to consider 
         # and the latter the corresponding Vcal values
         
-        VcThrs = range(VcThrMin, VcThrMax, int(step))
-        Vcals  = [] 
+        Vcals = range(VcalMin, VcalMax, int(step))
+        VcThrs  = [] 
         
-    
-        for VcThr in VcThrs:
-            bin = h.GetXaxis().FindBin(VcThr)
-            h_Vcal = h.ProjectionY("Vcal", bin, bin)
-            firstBin = h_Vcal.FindFirstBinAbove(0.5)
-            Vcals.append(h_Vcal.GetBinCenter(firstBin))
+        for Vcal in Vcals:
+            bin = h.GetYaxis().FindBin(Vcal)
+            h_VcThr= h.ProjectionX("VcThr", bin, bin)
+            firstBin = h_VcThr.FindFirstBinAbove(0.4)
+            VcThrs.append(h_VcThr.GetBinCenter(firstBin))
         
         VcThrArray = array('f', VcThrs)
         VcalArray = array('f', Vcals)
-        VcThrVcal_graph = ROOT.TGraph( len(VcThrArray), VcThrArray, VcalArray)
-        
+        VcThrVcal_graph = ROOT.TGraph( len(VcalArray), VcalArray, VcThrArray)
         VcThrVcal_graph.Fit("pol1", "Q")
         VcThrVcal_graph.SetTitle(cName)
-        VcThrVcal_graph.GetXaxis().SetTitle("VcThr")
-        VcThrVcal_graph.GetYaxis().SetTitle("Vcal")
-        
+        VcThrVcal_graph.GetXaxis().SetTitle("Vcal")
+        VcThrVcal_graph.GetYaxis().SetTitle("VcThr")
         fitRes = VcThrVcal_graph.GetFunction("pol1")
         
         ofile.write('\n%s   %.2f   %.2f   %.2f'%(cName, fitRes.GetParameter(0), fitRes.GetParameter(1), fitRes.GetChisquare()/fitRes.GetNDF(),))                      
-    
+        if(fitRes.GetChisquare()/fitRes.GetNDF() > 10.): failingRocs = failingRocs + 1
         if(savePlots == 'True'):
             if(not os.path.isdir("plots")): os.system('mkdir plots')
             c = ROOT.TCanvas(cName+'_fit')
             c.cd()
             VcThrVcal_graph.Draw("A*")
             c.Print("plots/" + cName+".pdf")
-
+    if(failingRocs > 0): print failingRocs, "failing ROCs in module: ", cName
 
 
 def checkROCthr(path):
@@ -122,4 +123,54 @@ def readHistoInfo(name):
     return a
 
 
+
+def createNewDACsettings(path):
+    filename = path + 'PixelConfigurationKey.txt'    
+    key, dac = 0, 0
+
+    # Open the file and look for the key 
+    try: 
+        f = open(filename, 'r')
+    except IOError:
+        print "Cannot open file ", filename
+    else:
+        lines = f.readlines()
+        key = lines[1].split()[-1]
+        print key 
+        # Open the configuration file and look for the key 
+        try: 
+           cfg = open(confpath)
+            
+        except IOError:
+            print "Cannot open ", confpath
+
+        else:
+            while True:
+                l = cfg.readline()
+                if(l.startswith("key")):
+                    if(l.split()[1] == str(key) ):
+                        pos = cfg.tell()
+                        print pos
+                        break
+                    
+            cfg.seek(pos)
+            while True:
+                l = cfg.readline()
+                if(l.startswith("dac")):
+                    dac = l.split()[1]
+                if(l.startswith("calib")):
+                    calib = l.split()[1]
+
+                    break
+            print "dummy"
+            print "key ",key
+            print "dac ",dac
+            print "calib ",calib
+                    
+#            cfg.seek(pos)
+
+   
+
+        
+        
 
